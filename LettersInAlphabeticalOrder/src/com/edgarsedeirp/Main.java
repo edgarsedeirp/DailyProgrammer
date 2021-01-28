c662196177946a4417a79f5136f65b7e474375a2
edgarsedeirp <sleepijs1@gmail.com>
2021-01-28 22:41:15 +0200

    Complete solution to challange

--- LettersInAlphabeticalOrder/src/com/edgarsedeirp/Main.java
+++ LettersInAlphabeticalOrder/src/com/edgarsedeirp/Main.java
@@ -1,8 +1,52 @@
 package com.edgarsedeirp;
 
+import java.nio.file.Paths;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> userInputList = readInputFromFile();
+
+        for (String originalString : userInputList) {
+            if (isStringInOrder(originalString)) {
+                System.out.println(originalString + " IN ORDER");
+            } else if (isStringReversed(originalString)) {
+                System.out.println(originalString + " REVERSE ORDER");
+            } else {
+                System.out.println(originalString + " NOT IN ORDER");
+            }
+        }
+    }
+
+    private static List<String> readInputFromFile() {
+        List<String> userInputList = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(Paths.get("Input.txt"))) {
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                userInputList.add(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return userInputList;
+    }
+
+    private static boolean isStringInOrder(String input) {
+        char[] stringToCheck = input.toCharArray();
+        Arrays.sort(stringToCheck);
+
+        return (input.equals(new String(stringToCheck)));
+    }
+
+    private static boolean isStringReversed(String input) {
+        String reversedString = new StringBuilder(input).reverse().toString();
+        char[] stringToCheck = reversedString.toCharArray();
+
+        Arrays.sort(stringToCheck);
+
+        return (reversedString.equals(new String(stringToCheck)));
     }
 }
