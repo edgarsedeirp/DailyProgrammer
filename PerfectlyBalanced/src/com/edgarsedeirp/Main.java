d568207e93a2af3b46ee0e0bbdcfbab99b1e54cb
edgarsedeirp <sleepijs1@gmail.com>
2021-03-20 12:13:19 +0200

    Solution to bonus challange

--- PerfectlyBalanced/src/com/edgarsedeirp/Main.java
+++ PerfectlyBalanced/src/com/edgarsedeirp/Main.java
@@ -4,24 +4,37 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> stringList = readInputFromFile();
+        List<String> initialChallangeInputList = readInputFromFile("InitialChallangeInput.txt");
+        List<String> bonusChallangeInputList = readInputFromFile("BonusChallangeInput.txt");
 
-        for (String string : stringList) {
-            System.out.println("\"" + string + "\"" + " => " + balanced(string));
-        }
+        initialChallangeInputList.forEach(input -> {
+            System.out.println("\"" + input + "\"" + " => " + balanced(input));
+        });
+
+        System.out.println("---------------------BONUS CHALLANGE---------------------");
+
+        bonusChallangeInputList.forEach(input -> {
+            System.out.println("\"" + input + "\"" + " => " + balanced_bonus(input));
+        });
+
     }
 
-    public static List<String> readInputFromFile(){
+    public static List<String> readInputFromFile(String filename) {
         List<String> inputFromFile = new ArrayList<>();
 
         try {
-            Scanner scanner = new Scanner(Paths.get("Input.txt"));
-            while(scanner.hasNextLine()){
+            Scanner scanner = new Scanner(Paths.get(filename));
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 inputFromFile.add(line);
             }
@@ -31,14 +44,33 @@ public class Main {
         return inputFromFile;
     }
 
-    public static boolean balanced(String input){
-       return (countOccurance(input, 'x') == countOccurance(input, 'y'));
+    public static boolean balanced_bonus(String input) {
+        Map<Character, Integer> frequencies = input.chars().boxed()
+                .collect(toMap(k -> (char) k.intValue(),
+                        v -> 1,             //1 occurance
+                        Integer::sum));
+
+        int[] result = frequencies.values().stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        int valueToCheck = result[0];
+        for (int i = 1; i < result.length; i++) {
+            if(valueToCheck != result[i]){
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    public static int countOccurance(String input, char character){
+    public static boolean balanced(String input) {
+        return (countOccurance(input, 'x') == countOccurance(input, 'y'));
+    }
+
+    public static int countOccurance(String input, char character) {
         return (int) input.chars()
                 .filter(c -> c == character)
                 .count();
     }
-
 }
