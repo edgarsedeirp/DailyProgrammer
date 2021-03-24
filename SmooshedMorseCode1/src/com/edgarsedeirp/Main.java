5eb0a43344bdd2004f1807cb25e9be338636d425
edgarsedeirp <sleepijs1@gmail.com>
2021-03-24 19:22:15 +0200

    Solution to Bonus 1.

--- SmooshedMorseCode1/src/com/edgarsedeirp/Main.java
+++ SmooshedMorseCode1/src/com/edgarsedeirp/Main.java
@@ -3,6 +3,8 @@ package com.edgarsedeirp;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -14,6 +16,9 @@ public class Main {
 	    inputFromFile.forEach(input -> {
             System.out.println("\"" + input + "\" => \"" + smorse(input) + "\"");
         });
+
+        System.out.println("----------------------BONUS----------------------");
+        smorseBonus();
     }
 
     private static String smorse(String input){
@@ -27,6 +32,31 @@ public class Main {
         }
 
         return stringBuilder.toString();
+    }
+
+    private static void smorseBonus(){
+        List<String> veryLargeInputFromFile = readInputFromFile("InputForBonusChallenge.txt");
+
+        List<String> veryLargeInputToMorse = veryLargeInputFromFile.stream()
+                .map(Main::smorse)
+                .collect((Collectors.toList()));
+
+        //Solution to Bonus 1.
+        Map<String, Long> frequencyMap =
+                veryLargeInputToMorse.stream()
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println("Solution to bonus 1 : " + getKeyFromValue(frequencyMap, 13L));
+
+    }
+
+    private static String getKeyFromValue(Map<String, Long> map, Long value) {
+        for (Map.Entry<String, Long> entry : map.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     private static List<String> readInputFromFile(String filename){
